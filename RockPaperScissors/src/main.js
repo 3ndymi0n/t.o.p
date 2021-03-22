0d5d8661414ce4b0f1aaa2cf1763af1fea8e89ac
3ndymi0n <endymi0n@systemdecay.info>
2021-03-22 23:49:48 +1100

    first commit

---
+++ RockPaperScissors/src/main.js
@@ -0,0 +1,53 @@
+function computerPlay() {
+    const moves = ["scissors", "paper", "rock"];
+    const randomChoice = Math.floor(Math.random() * 3);
+
+    return moves[randomChoice];
+};
+
+function gameRound(computerPlay, playerSelection) {
+    // Conditions to be declared the winner.
+
+    plays = {
+        "scissors": "paper",
+        "paper": "rock",
+        "rock": "scissors"
+    }
+
+    if (computerPlay == playerSelection){
+        return "Draw!";
+    }else if (plays[computerPlay] == playerSelection) {
+        return "Computer Wins";
+    } else {
+        return "Player Wins";
+    }
+        
+};
+
+function game() {
+    let roundCount = 1;
+    let computerWins = 0;
+    let playerWins = 0;
+
+    while((playerWins <= 3) && (computerWins <= 3)){
+        let computerTurn = computerPlay();
+        let playerTurn = prompt("Scissors, Paper or Rock?").toLowerCase();
+        let currentRound = gameRound(computerTurn, playerTurn);
+        console.log(currentRound);
+        if(currentRound == "Draw!") {
+            console.log("Round " + roundCount + " is a draw! Player has " + playerWins + " and computer has " + computerWins + ".");
+            roundCount++
+        }else if(currentRound == "Computer Wins"){
+            computerWins++
+            console.log("Round " + roundCount + " won by Computer! Player has " + playerWins + " and computer has " + computerWins + ".");
+            roundCount++
+        }else if(currentRound == "Player Wins") {
+            playerWins++
+            console.log("Round " + roundCount + " won by Player! Player has " + playerWins + " and computer has " + computerWins + ".");
+            roundCount++
+        }
+        
+    }
+}
+
+console.log(game());
